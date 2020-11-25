293f6cce3ab2b9df7b29742b8b61dd586389b4aa
codershona <falgunimst95@gmail.com>
2020-11-25 16:13:24 +0100

    react component: creating a function component with constructor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 class Car extends Component {
+	constructor() {
+		super();
+		this.state = { tracks: "yellow" };
+	}
   render() {
-    return <h2>Hi, I have a Car!</h2>;
+    return <h2>Hi, I have a { this.state.tracks } Car!</h2>;
   }
 }
 
