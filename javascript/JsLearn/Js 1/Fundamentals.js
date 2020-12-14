e1a9d5ee98f820338d994499a6ee12224109327c
codershona <falgunimst95@gmail.com>
2020-12-14 20:13:48 +0100

    learning function left need to learn

--- javascript/JsLearn/Js 1/Fundamentals.js
+++ javascript/JsLearn/Js 1/Fundamentals.js
@@ -916,6 +916,170 @@
 
 // Functions :
 
+// Function Declaration
+
+// function showMessage() {
+//   alert( 'Hello everyone!' );
+// }
+
+// showMessage();
+// showMessage();
+
+// function showAlert() {
+// 	alert('Jsas');
+// }
+
+// showAlert();
+
+
+
+// // Local variables :
+
+// function showAlerts() {
+//   let messages = "Hello, I'm JavaScript!......"; // local variable
+
+//   alert( messages );
+// }
+
+// showAlerts(); // Hello, I'm JavaScript!
+
+// alert( messages ); // <-- Error! The variable is local to the function
+
+
+
+let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+showMessage(); // Hello, John
+
+
+// method 2
+
+
+let userName = 'John';
+
+function showMessage() {
+  userName = "Bob"; // (1) changed the outer variable
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // John before the function call
+
+showMessage();
+
+alert( userName ); // Bob, the value was modified by the function
+
+
+
+// method 3
+
+// Parameters :
+
+
+
+function showMessage(from, text) { // arguments: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+showMessage('Ann', "What's up?"); // Ann: What's up? (**)
+
+
+// method 4
+
+function showMessage(from, text) {
+
+  from = '*' + from + '*'; // make "from" look nicer
+
+  alert( from + ': ' + text );
+}
+
+let from = "Ann";
+
+showMessage(from, "Hello"); // *Ann*: Hello
+
+// the value of "from" is the same, the function modified a local copy
+alert( from ); // Ann
+
+// default values
+
+function showMessage(from, text = "no text given") {
+  alert( from + ": " + text );
+}
+
+showMessage("Ann"); // Ann: no text given
+
+// method 5
+
+function showMessage(text) {
+  if (text === undefined) {
+    text = 'empty message';
+  }
+
+  alert(text);
+}
+
+showMessage(); // empty message
+
+// method 6
+
+// if there's no "count" parameter, show "unknown"
+function showCount(count) {
+  alert(count ?? "unknown");
+}
+
+showCount(0); // 0
+showCount(null); // unknown
+showCount(); // unknown
+
+
+// Returning a value
+
+
+function sum(a, b) {
+  return a + b;
+}
+
+let result = sum(1, 2);
+alert( result ); // 3
+
+
+// method 8
+function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm('Do you have permission from your parents?');
+  }
+}
+
+let age = prompt('How old are you?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Access granted' );
+} else {
+  alert( 'Access denied' );
+}
+
+
+// method 9
+
+function doNothing() {
+  return;
+}
+
+alert( doNothing() === undefined ); // true
+
+// CHAPTER : Function expressions :
+
+
+
 
 
 
