c569d774fbe13b5f4c6ad120258ca13fe451fa5a
codershona <falgunimst95@gmail.com>
2020-12-21 22:46:28 +0100

    Constructor, operator new

--- javascript/JsLearn/Js 1/Object.js
+++ javascript/JsLearn/Js 1/Object.js
@@ -296,15 +296,86 @@
 
 // Constructor, operator "new" :
 
-function User(name) {
-  this.name = name;
-  this.isAdmin = false;
-}
+// function User(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
 
-let user = new User("Jack");
+// let user = new User("Jack");
 
-alert(user.name); // Jack
-alert(user.isAdmin); // false
+// alert(user.name); // Jack
+// alert(user.isAdmin); // false
+
+// function Person(id) {
+// 	this.id = id;
+// 	this.idPanel = false;
+// }
+
+// let person = new Person("Rina");
+// alert(person.id);
+// alert(person.isPanel);
+
+
+// function Person() {
+//   alert(new.target);
+// }
+
+// // without "new":
+// Person(); // undefined
+
+// // with "new":
+// new Person(); // function User { ... }
+
+
+// function Person(id) {
+//   if (!new.target) { // if you run me without new
+//     return new Person(id); // ...I will add new for you
+//   }
+
+//   this.id = id;
+// }
+
+// let john = Person("John"); // redirects call to new User
+// alert(john.id); // John
+
+
+// function Calculator() {
+
+//   this.read = function() {
+//     this.a = +prompt('a?', 0);
+//     this.b = +prompt('b?', 0);
+//   };
+
+//   this.sum = function() {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function() {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+
+
+// function Accumulator(startingValue) {
+//   this.value = startingValue;
+
+//   this.read = function() {
+//     this.value += +prompt('How much to add?', 0);
+//   };
+
+// }
+
+// let accumulator = new Accumulator(1);
+// accumulator.read();
+// accumulator.read();
+// alert(accumulator.value);
+
 
 
 
