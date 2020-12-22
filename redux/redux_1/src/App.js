74a3e4147b8d767acab4ad332955e72f84833be9
codershona <falgunimst95@gmail.com>
2020-12-22 21:43:35 +0100

    learning redux part 1: connecting redux into react

--- redux/redux_1/src/App.js
+++ redux/redux_1/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 import './App.css';
 // import { createStore } from "redux";
+import { Provider } from "react-redux";
+import store from './store';
+import Count from './components/Count';
 
 
 class App extends Component {
   render() {
+    
 
 
     return(
+      <Provider store={ store }>
       <div className="App">
         <h1>
           Learning Redux From Crash
         </h1>
 
+        <Count />
+
       </div>
+      </Provider>
     );
   }
 }
