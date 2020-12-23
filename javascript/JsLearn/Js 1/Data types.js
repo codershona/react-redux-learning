6d629c7d1586b356c7fec6fefd7b0fed2a97bba5
codershona <falgunimst95@gmail.com>
2020-12-23 14:28:20 +0100

    learning data types

--- javascript/JsLearn/Js 1/Data types.js
+++ javascript/JsLearn/Js 1/Data types.js
@@ -1,2 +1,52 @@
 // Data types:
 
+
+// Methods of primitives:
+// let str = "Hello";
+
+// alert( str.toUpperCase() ); // HELLO
+
+// let person = "Hello John";
+// alert( person.toUpperCase() );
+
+// method 2
+
+// let n = 1.23456;
+
+// alert( n.toFixed(2) ); // 1.23
+
+// let x = 1.3333;
+// alert(x.toFixed(3));
+
+// method 3
+
+// let zero = new Number(0);
+
+// if (zero) { // zero is true, because it's an object
+//   alert( "zero is truthy!?!" );
+// }
+
+// let hero = new Number(1);
+// if (hero) {
+//     alert("hero is testing");
+// }
+
+
+
+// method 4
+
+// let str = "Hello";
+
+// str.test = 5; // (*)
+
+// alert(str.test);
+
+// let num = "Hello Js";
+// num.test = 6;
+// alert(num.test);
+
+
+// Numbers:
+
+// method 1:
+
