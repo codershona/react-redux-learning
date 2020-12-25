f8fdc3641afa9502214d881e156c4d6c766916ba
codershona <falgunimst95@gmail.com>
2020-12-25 18:11:14 +0100

    learning using state inot component

--- redux/react_1/src/Components/Title/Title.js
+++ redux/react_1/src/Components/Title/Title.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 
 
 class Title extends React.Component {
+   
+    constructor(props) {
+        super(props)
+
+        /* this.number = 0 */
+        this.state = {
+            number: 0
+        }
+    }
+
+    counter() {
+        /* this.number = this.number + 1; */
+        /* this.state.number = this.number + 1 */
+
+        this.setState({
+            number: this.state.number + 1
+        })
+
+        console.log(this.state.number)
+    }
+
       
     render() {
         return(
-            <h5>
+            /* <h5>
                 The Title Component was : {this.props.name} and Email address: ({this.props.email})
-            </h5>
+            </h5> */
+        <div>
+            <p>
+                { this.state.number }
+                <button onClick={ () => this.counter() }>
+                CLICK HERE
+                </button>
+            </p>
+        </div>
+
 
         );
     }
