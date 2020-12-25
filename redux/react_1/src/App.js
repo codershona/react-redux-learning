aa3cb380e4efb7ed9cd2047bfb4bd7de89c636c2
codershona <falgunimst95@gmail.com>
2020-12-25 17:29:04 +0100

    react project setup done

--- redux/react_1/src/App.js
+++ redux/react_1/src/App.js
@@ -1,12 +1,15 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Learning React Crash Course</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className = "App">
+     <h1>
+       Learning React Crash Course
+     </h1>
+      </div>
+    )
+  }
 }
-
 export default App;
