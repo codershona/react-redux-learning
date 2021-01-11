1205c1a4183acef4b85b85b1b623fcaef7e8dc6f
codershona <falgunimst95@gmail.com>
2021-01-11 22:08:37 +0100

    starting learning hooks

--- redux/react_contextapi_hooks/src/components/app.jsx
+++ redux/react_contextapi_hooks/src/components/app.jsx
@@ -16,7 +16,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ redux/react_contextapi_hooks/src/components/hooks.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const MyHook = () => {
+    return(
+        <div>
+            <h3>
+                This is hooks basics.
+            </h3>
+        </div>
+    );
+};
+
+
+export default MyHook;
