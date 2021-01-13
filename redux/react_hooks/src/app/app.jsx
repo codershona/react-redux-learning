ebc231f8f07a8fa201f674bf8bda7e8563964abf
codershona <falgunimst95@gmail.com>
2021-01-13 14:25:46 +0100

    project setup lefted

---
+++ redux/react_hooks/src/app/app.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Counter from '../componnets/counter';
+
+const App = () => {
+  const [counting, setCounting] = useState(0);
+
+  return(
+    <div className='container my-3'>
+      
+    </div>
+
+  );
+};
+
+
+export default App;
