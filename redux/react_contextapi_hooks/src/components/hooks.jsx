a070b888ec5363450399a7e3344245ae452c267b
codershona <falgunimst95@gmail.com>
2021-01-13 14:11:09 +0100

    fetching api with useeffect done

--- redux/react_contextapi_hooks/src/components/hooks.jsx
+++ redux/react_contextapi_hooks/src/components/hooks.jsx
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MyHook = () => {
-    const [counts, setCount] = useState(100);
+    const [counts, setCounts] = useState(100);
+
+    const [ages, setAges] = useState(24);
+
+    const [apiposts, setApiPost] = useState({});
+
+    useEffect(() => {
+        console.log('Count Updated it!', counts);
+
+        return () => {
+         console.log('UNSUBSCRIBED!');
+        };
+    }, [counts, ages]);
+
+    useEffect(() => {
+      console.log('I have some values', ages);
+      // document.title = 'This has some values ' + ages;
+    }, [ages]);
+
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/posts/1')
+        .then(res => res.json())
+        .then(data => setApiPost(data))
+        .catch(err => console.log(err))
+    }, []);
+
+
 
     return(
         <div>
@@ -11,15 +37,39 @@ const MyHook = () => {
             <h1>
                 Count: {counts}
             </h1>
-            <button onClick={() => setCount(counts + 1)}>
+            <button onClick={() => setCounts(counts + 1)}>
                 Increase
             </button>
-            <button onClick={() => setCount(prev => prev -1)}>
+            <button onClick={() => setCounts(prev => prev -1)}>
                 Decrease
             </button>
-            <button onClick={() => setCount(100)}>
+            <button onClick={() => setCounts(100)}>
                 Re-establish
             </button>
+            <div>
+                <hr />
+                <h2>
+                    Her Ages was: {ages}
+                </h2>
+                <button onClick={() => setAges(ages + 1)}>
+                    Increase Ages
+                </button>
+            </div>
+            <div>
+                {Object.keys(apiposts).length > 0 && (
+                    <>
+                    <h3>
+                        Title: {apiposts.title}
+                    </h3>
+                    <p>
+                        <strong>
+                            BODY: {apiposts.body}
+                        </strong>
+
+                    </p>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
