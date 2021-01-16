9237297f7e13f6bc37b77795f6d9de4f580c7d82
codershona <falgunimst95@gmail.com>
2021-01-16 17:08:43 +0100

    working with let var const and template strings

---
+++ es6/dist/bundle.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+eval("//  * let vs const vs var :\n// const y = 20;\n// y = 23;\n// console.log(y);\n// const array = [];\n// array.push(20);\n// array.push(24);\n// // array = [];\n// console.log(array);\n// let y = 20;\n// console.log(y);\n// if (true) {\n//     let y = 30;\n//     console.log(y);\n// }\n// console.log(y);\n// var y = 20;\n// console.log(y);\n// if (true) {\n//     var y = 30;\n//     console.log(y);\n// }\n// console.log(y);\nfunction Demo() {\n  {\n    var a = 22;\n    {\n      var _a = 28;\n      console.log(_a); // a = 40;\n    }\n    a = 40;\n    console.log(a);\n  } // console.log(a);\n}\n\nDemo(); //  * Temple String and Multiline String :\n//  ----- Example -------:\n// import { ABC } from './test';\n// import '../styles/index.scss';\n// const fn = () => {\n//     console.log('Hey sds')\n// }\n// fn()\n// // npm start\n// // npm run build\n// \"start\": \"webpack serve\",\n// \"build\": \"webpack\"\n// console.log(ABC)\n// class X {\n//     a = 10\n//     ABC() {\n//         console.log(a)\n//     }\n// }\n// let a = new X()\n// a.ABC()\n\n//# sourceURL=webpack://es6/./src/scripts/index.js?");
+/******/ })()
+;
--- es6/src/scripts/index.js
+++ es6/src/scripts/index.js
@@ -1,26 +1,111 @@
-import { ABC } from './test';
+//  * let vs const vs var :
+// const y = 20;
 
-import '../styles/index.scss';
+// y = 23;
 
-
-const fn = () => {
-    console.log('Hey sds')
-}
-
-fn()
+// console.log(y);
 
 
-console.log(ABC)
 
-class X {
-    a = 10
+// const array = [];
+// array.push(20);
+// array.push(24);
 
-    ABC() {
-        console.log(a)
-    }
-}
+// // array = [];
 
-let a = new X()
-a.ABC()
+// console.log(array);
+
+// let y = 20;
+// console.log(y);
+
+// if (true) {
+//     let y = 30;
+//     console.log(y);
+// }
+
+// console.log(y);
+
+
+
+// var y = 20;
+// console.log(y);
+
+// if (true) {
+//     var y = 30;
+//     console.log(y);
+// }
+
+// console.log(y);
+
+
+// function Demo() {
+//     {
+//        let a = 22;
+//     {
+//         const a = 28;
+//         console.log(a);
+//         // a = 40;
+//     }
+//     a = 40;
+//     console.log(a);
+//  }
+//  // console.log(a);
+// }
+
+
+// Demo();
+
+//  * Temple String and Multiline String :
+
+//   const Barbies = 'This is Barbie doll';
+//   const episodes = 236;
+//   const emails = 'barbie@test.com';
+
+// // const Cartoon_Network = 'Cartoon Name: ' + Barbie + 'Cartoon episode: ' + episode + 'We Can Email in ' + email
+
+// const Cartoon_Networks = `
+//    Cartoon's Name: ${Barbies};
+//    Cartoon's Episode: ${episodes};
+//    We can email: ${emails}
+
+// `
+
+// console.log(Cartoon_Networks);
+
+
+
+//  ----- Example -------:
+
+
+// import { ABC } from './test';
+
+// import '../styles/index.scss';
+
+
+// const fn = () => {
+//     console.log('Hey sds')
+// }
+
+// fn()
+
+// // npm start
+// // npm run build
+
+// "start": "webpack serve",
+// "build": "webpack"
+
+
+// console.log(ABC)
+
+// class X {
+//     a = 10
+
+//     ABC() {
+//         console.log(a)
+//     }
+// }
+
+// let a = new X()
+// a.ABC()
 
 
