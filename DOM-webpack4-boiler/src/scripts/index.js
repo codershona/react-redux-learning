24086e1ddc48214021778474689ee404eb5715ce
codershona <falgunimst95@gmail.com>
2021-02-04 17:33:34 +0100

    creating tasks method using event and adding createnewtasks function

--- DOM-webpack4-boiler/src/scripts/index.js
+++ DOM-webpack4-boiler/src/scripts/index.js
@@ -1,2 +1,24 @@
 import '../styles/index.scss';
 
+window.onload = function() {
+
+   const taskField = document.querySelector('#tasksField')
+
+   const addTasksButton = document.querySelector('#addTasksButton')
+
+   const all_TaskParent = document.querySelector('#all_Task')
+
+   tasksField.addEventListener('keypress', function() {
+       if (event.keyCode === 13) {
+        //    console.log('Type New Pressed')
+        createNewTasks(all_TaskParent, event.target.value)
+        this.value = ''
+
+       }
+   })
+
+}
+
+function createNewTasks(parent, task) {
+    console.log(task)
+}
