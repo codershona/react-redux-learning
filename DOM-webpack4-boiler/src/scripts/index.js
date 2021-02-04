2a5f240d8acebe6e2700b3d4d337be8821110fd9
codershona <falgunimst95@gmail.com>
2021-02-04 17:43:48 +0100

    creating tasks with using appendchild and single tasks

--- DOM-webpack4-boiler/src/scripts/index.js
+++ DOM-webpack4-boiler/src/scripts/index.js
@@ -19,6 +19,48 @@ window.onload = function() {
 
 }
 
-function createNewTasks(parent, task) {
-    console.log(task)
+function createNewTasks(parent, tasks) {
+    // console.log(tasks)
+
+    let col = create({'class': 'col-sm-3'})
+
+    let single_Tasks = create({'class': 'single_tasks'})
+
+    let single_Task_P = create('p')
+
+    single_Task_P.innerHTML = 'tasks'
+    single_Tasks.appendChild(single_Task_P)
+
+    col.appendChild(single_Tasks)
+    parent.appendChild(col)
+    
+}
+
+
+
+
+window.create = function () {
+
+    if (arguments.length === 0) {
+        return document.createElement('div');
+    }
+
+    if (arguments.length === 1 && typeof arguments[0] != 'object') {
+        return document.createElement(arguments[0]);
+    }
+
+    var tag = arguments[0];
+    var attr = arguments[1] || arguments[0];
+
+    if (arguments.length === 1 && typeof arguments[0] === 'object') {
+        tag = 'div';
+    }
+
+    var element = document.createElement(tag);
+
+    for (var i in attr) {
+        element.setAttribute(i, attr[i]);
+    }
+
+    return element;
 }
