6ec92959c9f34765cdf52dc026725ada1bb2753b
codershona <falgunimst95@gmail.com>
2021-02-18 19:18:26 +0100

    first class function learning started

---
+++ UDEMY/learning_2/basics.js
@@ -0,0 +1,37 @@
+// functional programming javascript
+// functional programming means all functions about input x-----> Function f: --------> output f(x)
+
+var name = 'Barbie doll';
+var greeting = "Hey how are you?";
+
+console.log(greeting + ', ' + 'Hey how are you?');
+
+
+// imperative vs Declarative
+
+function greetingOne(name, greeting) {
+    return(greeting + ', ' + 'Hey how are you?');
+}
+// This is declarative
+// This is a Pure Functions
+greetingOne(name, greeting);
+
+// keep it simple, stupid(KISS)
+// pure functional
+// function component
+// shared state
+// mutable data
+// side effects
+
+// follow this logic
+// input, x = 3
+// Function, f: x2
+// OUTPUT, f(x) = 9
+// Input =====> FUNCTION g: x + 1 ======> (COMPOSITION) ====> OUTPUT g(f(x)) = 10
+
+
+
+
+
+
+
