10b7ff8c660e249979bff0e543f21e03902ba6fd
codershona <falgunimst95@gmail.com>
2021-02-23 18:10:49 +0100

    working with return statement injs is done

--- UDEMY/learning_2/return_statement.js
+++ UDEMY/learning_2/return_statement.js
@@ -2,23 +2,23 @@
 
 
 // method 1
-// function name(fullName, surName, identity) {
-//     var output;
+function name(fullName, surName, identity) {
+    var output;
 
-//     if (identity === 'others') {
-//         // var output = 'Not prefer to say, ' + fullName + ' ' + surName;
-//         output = 'Not prefer to say, ' + fullName + ' ' + surName;
-//         // return output;
-//     } else if (identity === 'women') {
-//         // var output = 'Ms, ' + fullName + ' ' + surName;
-//         output = 'Ms, ' + fullName + ' ' + surName;
-//         // return output;
-//     }
-//     return output;
-// }
+    if (identity === 'others') {
+        // var output = 'Not prefer to say, ' + fullName + ' ' + surName;
+        output = 'Not prefer to say, ' + fullName + ' ' + surName;
+        // return output;
+    } else if (identity === 'women') {
+        // var output = 'Ms, ' + fullName + ' ' + surName;
+        output = 'Ms, ' + fullName + ' ' + surName;
+        // return output;
+    }
+    return output;
+}
 
-// var result = name('Barbie', 'Doll','women');
-// console.log(result);
+var result = name('Barbie', 'Doll','women');
+console.log(result);
 
 // method 2
 
