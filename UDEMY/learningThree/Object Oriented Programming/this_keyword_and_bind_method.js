ebbeb56caf8dc4ece412e744814b300c845dc955
codershona <falgunimst95@gmail.com>
2021-02-25 13:03:26 +0100

    currently studying This Keyword and Bind Method

--- UDEMY/learningThree/Object Oriented Programming/this_keyword_and_bind_method.js
+++ UDEMY/learningThree/Object Oriented Programming/this_keyword_and_bind_method.js
@@ -4,6 +4,22 @@
 
 // method 1:
 
+var object = {
+    name: 'Jenny Penny',
+
+    print: function() {
+        console.log(this);
+    }
+}
+
+function new_Function() {
+    console.log(this);
+}
+
+object.print();
+
+// method 2:
+
 
 
 
