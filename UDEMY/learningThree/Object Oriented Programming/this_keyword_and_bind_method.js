b94f166ac66072e960ac8097f9f70ab137f0899a
codershona <falgunimst95@gmail.com>
2021-02-28 18:05:42 +0100

    This keyword and bind method done

--- UDEMY/learningThree/Object Oriented Programming/this_keyword_and_bind_method.js
+++ UDEMY/learningThree/Object Oriented Programming/this_keyword_and_bind_method.js
@@ -1,25 +1,95 @@
 // This Keyword and Bind Method :
 
-
-
 // method 1:
 
-var object = {
-    name: 'Jenny Penny',
+// var object = {
+//     name: 'Jenny Penny',
 
-    print: function() {
-        console.log(this);
-    }
-}
+//     print: function() {
+//         console.log(this);
+//     }
+// }
 
-function new_Function() {
-    console.log(this);
-}
+// function new_Function() {
+//     // console.log(this);
+//     function inner() {
+//         console.log(this);
+//     }
+//     new inner();
 
-object.print();
+// }
+
+// object.print();
+
+// new_Function(); // if we want to call the function in the console
+
+
 
 // method 2:
 
+// var bindOne = {
+//     names: 'Jennifer Winget',
+//     print: function() {
+//         console.log('This is keyword method ' + this.names);
+//     }
+// }
+
+// var mybindOne = bindOne.names;
+// console.log(mybindOne);
+// bindOne.print();
+
+// var mybindOne = bindOne.print;
+// console.log(mybindOne);
+// mybindOne();
+// var mybindOne = function() {
+//     console.log(this);
+//     console.log('This is also ' + this.names);
+// }
+
+// console.log(mybindOne);
+
+// mybindOne();
+
+
+// method 3
+
+// function addition(num) {
+//     console.log(this);
+//   return this.value + num;
+
+// }
+
+// var object = {
+//     value: 211
+// }
+
+// var objectOne = {
+//     value: 330
+// }
+
+// var outcome = addition.bind(object, 200);
+// var output = outcome(500);
+
+// console.log(output);
+
+
+// method 4
+
+
+var keyword = {
+    id: 'oneTwoThree',
+
+    print: function() {
+        // var id = this.id;
+        setTimeout(function() {
+            console.log(this);
+            console.log('Functions are bind ' + this.id);
+        }.bind(this), 2400);
+    }
+}
+
+// keyword
+// keyword.print();
 
 
 
