1bd6eaca3fcdde4cea3c437d219fe5b03ee25f0d
codershona <falgunimst95@gmail.com>
2021-03-07 19:47:41 +0100

    working with core types and diving deeper

--- typescript/part 1/apps.js
+++ typescript/part 1/apps.js
@@ -1,2 +1,28 @@
-var message = "This is Typescript";
-console.log(message);
+// let message = "This is Typescript"
+// console.log(message);
+// Run this into the terminal : tsc app.ts
+// Udemy Practices
+// Working with types & Core types or diving deeper
+// Method 1
+console.clear();
+var numbers1Input = document.getElementById('numbers1');
+var numbers2Input = document.getElementById('numbers2');
+var buttonElement = document.querySelector('button');
+function addition(x, y) {
+    return x + y;
+}
+// function printingMyResult(outcome: any) {
+function printingMyResult(outcome) {
+    console.log(outcome);
+    //return undefined;
+}
+buttonElement.addEventListener('click', function () {
+    var numbers1 = +numbers1Input.value;
+    var numbers2 = +numbers2Input.value;
+    var outcome = addition(numbers1, numbers2);
+    printingMyResult(outcome);
+});
+// const outcome = addition(6, 7);
+// let isDoing = false;
+// printingMyResult(outcome);
+// console.log(outcome);
--- typescript/part 1/apps.ts
+++ typescript/part 1/apps.ts
@@ -1,6 +1,51 @@
-let message = "This is Typescript"
-console.log(message);
+// let message = "This is Typescript"
+// console.log(message);
 // Run this into the terminal : tsc app.ts
+
+// Udemy Practices
+
+
+// Working with types & Core types or diving deeper
+// Method 1
+
+console.clear();
+
+
+const numbers1Input = document.getElementById('numbers1') as HTMLInputElement;
+const numbers2Input = <HTMLInputElement>document.getElementById('numbers2');
+
+const buttonElement = document.querySelector('button');
+
+function addition(x : number, y : number) {
+    return x + y;
+}
+
+
+
+// function printingMyResult(outcome: any) {
+    function printingMyResult(outcome) {
+    console.log(outcome);
+    //return undefined;
+}
+
+buttonElement.addEventListener('click', () => {
+    const numbers1 = +numbers1Input.value;
+    const numbers2 = +numbers2Input.value;
+    const outcome = addition(numbers1, numbers2);
+    const outcomeContainer: object = {
+        res: outcome
+    };
+    printingMyResult(outcomeContainer);
+});
+
+// const outcome = addition(6, 7);
+// let isDoing = false;
+
+// printingMyResult(outcome);
+
+// console.log(outcome);
+
+
 
 
 
