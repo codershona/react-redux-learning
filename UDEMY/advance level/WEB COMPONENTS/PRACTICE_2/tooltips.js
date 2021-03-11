89d941383d2e74eed07165baf7964348a5020860
codershona <falgunimst95@gmail.com>
2021-03-11 18:39:40 +0100

    working with listening events into inside the compoents

--- UDEMY/advance level/WEB COMPONENTS/PRACTICE_2/tooltips.js
+++ UDEMY/advance level/WEB COMPONENTS/PRACTICE_2/tooltips.js
@@ -5,15 +5,33 @@ class Tooltips extends HTMLElement {
         // toolTipIcons.textContent = ' (?)';
         // this.appendChild(toolTipIcons);
         // console.log("Learning web components");
+
+        this._toolTipContainers;
     }
 
   // using connected Call back () method so that we could access in the DOM
     connectedCallback() {
-        const toolTipIcons =  document.createElement('span');
+        const toolTipIcons = document.createElement('span');
         toolTipIcons.textContent = ' (?)';
+        toolTipIcons.addEventListener('mouseenter', this._showTooltips.bind(this));
+        toolTipIcons.addEventListener('mouseleave', this._hideTooltips.bind(this));
         this.appendChild(toolTipIcons);
 
     }
+
+    _showTooltips() {
+     this._toolTipContainers = document.createElement('div');
+     this._toolTipContainers.textContent = 'I am writing ToolTip Texts';
+     this.appendChild(this._toolTipContainers);
+
+    }
+
+    // hiding tooltips and it will hide the data
+     _hideTooltips() {
+        this.removeChild(this._toolTipContainers);
+
+
+     }
 
 }
 
