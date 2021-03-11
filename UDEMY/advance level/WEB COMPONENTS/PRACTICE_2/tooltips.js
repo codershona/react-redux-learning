c047f7fbe48ea7cde81f67b6f7a0b7f4ef234258
codershona <falgunimst95@gmail.com>
2021-03-11 18:20:30 +0100

    Using connectedCallBack for the DOM Access

--- UDEMY/advance level/WEB COMPONENTS/PRACTICE_2/tooltips.js
+++ UDEMY/advance level/WEB COMPONENTS/PRACTICE_2/tooltips.js
@@ -1,10 +1,18 @@
 class Tooltips extends HTMLElement {
     constructor() {
         super();
+        // const toolTipIcons =  document.createElement('span');
+        // toolTipIcons.textContent = ' (?)';
+        // this.appendChild(toolTipIcons);
+        // console.log("Learning web components");
+    }
+
+  // using connected Call back () method so that we could access in the DOM
+    connectedCallback() {
         const toolTipIcons =  document.createElement('span');
         toolTipIcons.textContent = ' (?)';
         this.appendChild(toolTipIcons);
-        // console.log("Learning web components");
+
     }
 
 }
