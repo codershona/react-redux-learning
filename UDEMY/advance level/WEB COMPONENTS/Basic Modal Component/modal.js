6ea27b4c37dbfa74d29b61a826c056d04ad036e7
codershona <falgunimst95@gmail.com>
2021-03-17 18:25:39 +0100

    public methods and properties

--- UDEMY/advance level/WEB COMPONENTS/Basic Modal Component/modal.js
+++ UDEMY/advance level/WEB COMPONENTS/Basic Modal Component/modal.js
@@ -2,6 +2,8 @@ class Modals extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
+
          this.shadowRoot.innerHTML = `
          <style>
          #backdrop {
@@ -90,25 +92,37 @@ class Modals extends HTMLElement {
 
     }
 
- //   attributeChangedCallback(name, oldValue, newValue) {
+   attributeChangedCallback(name, oldValue, newValue) {
 
 
         // opening the modal via css:
         // if (name === 'opened') {
-        //     if (this.hasAttribute('opened')) {
-        //         this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-        //         this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-        //         this.shadowRoot.querySelector('#modal').style.opacity = 1;
-        //         this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-        //     }
+            if (this.hasAttribute('opened')) {
 
-        // }
-    // }
+                this.isOpen = true;
+                // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+                // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+                // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+                // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+            // }
+
+        } else {
+            this.isOpen = false;
+       }
+    }
 
 
-    // static get observedAttributes() {
-    //     return ['opened'];
-    // }
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+
+    open() {
+        this.setAttribute('opened', '');
+        // this.isOpen; // this will define as false
+        this.isOpen = true;
+
+    }
 
 }
 
